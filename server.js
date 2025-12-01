58fc4eeebeaa2b7ae20523ca08248ab90a900d88
vectorCodes <ansarihassan0703@gmail.com>
2025-12-01 12:05:23 +0530

    intialize server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const app = express();
+
+const distPath = path.join(__dirname, "dist");
+
+app.use(express.static(distPath, { index: "index.html" }));
+
+app.use((req, res) => {
+  res.sendFile(path.join(distPath, "index.html"));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
