61b2f2b174706a8323bc33eaabf5562f3d4a9e3b
vectorCodes <ansarihassan0703@gmail.com>
2025-12-01 12:14:24 +0530

    Add description of the app in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,9 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <p>
+          This is a react app deployed to a server using Vite and React.
+        </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
